eb243416bc7e89cd59fb250cb1d38627c4314cce
da.fomenko <doctorhaus09@gmail.com>
2022-12-07 10:39:40 +0500

    2 дз

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 DATA = {
     'omlet': {
@@ -16,15 +17,15 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
+
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def get_recipes(request, food):
+    servings = int(request.GET.get('servings', 1))
+    new_amount = {}
+    for key in DATA[food]:
+        new_amount[key] = DATA[food][key]*servings
+    return render(request, 'calculator/index.html', context={'recipe': new_amount, 'food': food})
+
+
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import get_recipes
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<food>/', get_recipes)
 ]
