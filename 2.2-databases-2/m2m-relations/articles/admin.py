5936cf30578da9b743dd76e63266e594c75fd54e
da.fomenko <doctorhaus09@gmail.com>
2023-02-18 17:09:13 +0500

    дз 4.2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+
+        if len(self.forms) == 0:
+            raise ValidationError('Не указаны теги!')
+
+        count_main = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                count_main += 1
+        if count_main > 1:
+            raise ValidationError('Должен быть один главный тег')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 0
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'published_at']
+    inlines = [ScopeInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+
